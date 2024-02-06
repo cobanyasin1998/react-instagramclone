dc4ec487fd418e983a1e2151916a12513f18c45f
Yasin Çoban <cobanyasin1998@gmail.com>
2024-02-06 20:25:36 +0300

    home page and post page

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import "./post.css";
+import Avatar from "@mui/material/Avatar";
+import AvatarImg from "../../img/avatar/avatar.png";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <div className="post-wrapper">
+      <div className="post-header">
+        <div className="post-header-left">
+          <a href="/">
+            <Avatar
+              alt="Avatar Image"
+              src={AvatarImg}
+              sx={{ width: 28, height: 28 }}
+            />
+          </a>
+          <a href="/" className="profile-username">
+            yasiincoban
+          </a>
+        </div>
+        <div className="post-header-right">
+          <button>
+            <MoreHorizIcon />
+          </button>
+        </div>
+      </div>
+      <div className="post-img"></div>
+      <div className="post-bottom"></div>
+    </div>
+  );
 };
 
 export default Post;
