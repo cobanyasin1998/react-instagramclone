0d857faf872b1ca5c219da533345db1499eb88b1
Yasin Çoban <cobanyasin1998@gmail.com>
2024-02-15 00:57:06 +0300

    backend continue

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,2 +1,38 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
+
+//register
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+      bio: req.body.bio,
+      profilePicture: req.body.profilePicture,
+      fullName: req.body.fullName,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("Username or password is wrong");
+    const validPassword = await bcryptjs.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("Username or password is wrong");
+    res.status(200).json(user);
+  } catch {}
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const authRoute = require("./routes/auth.js");
+const usersRoute = require("./routes/users.js");
 
 const port = 5000;
 
@@ -15,6 +19,10 @@ const connectDB = async () => {
     console.error(error);
   }
 };
+app.use(express.json());
+app.use(morgan("common"));
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
 
 app.listen(port, () => {
   connectDB();
